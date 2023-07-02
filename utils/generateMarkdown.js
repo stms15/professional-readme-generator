c7bdb2a08fc7a7bce4d6b8b180bd1740b45001d5
Sarah S <stms15@mun.ca>
2023-07-02 18:26:39 -0230

    added questions section & fixed other input

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,8 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "") {
-    const licenseName = license.replace(" ", "_");
+    const licenseName = license.replaceAll(" ", "_");
     const badgeURL = `[![${license} badge](https://img.shields.io/badge/License-${licenseName}-lightblue)](${renderLicenseLink(
       license
     )})`;
@@ -12,7 +12,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === "GNU AGPLv3") {
@@ -36,7 +36,7 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== "") {
@@ -44,19 +44,47 @@ function renderLicenseSection(license) {
       "## License\n\n" +
       `The license for this application is ${license}. For more information about this license, click the badge or go to this link: ${renderLicenseLink(
         license
-      )}`
+      )}.`
     );
   } else {
     return "";
   }
 }
 
+// Create the list of sections for the Table Of Contents
+function renderTableOfContents(sections) {
+  let list = "";
+  for (let section of sections) {
+    list +=
+      "- [" +
+      section +
+      "](#" +
+      section.toLowerCase().replaceAll(" ", "-") +
+      ")\n";
+  }
+  return list;
+}
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  // Figure out what sections will be included
+  let sectionsList = ["Installation", "Usage"];
+  if (data.license !== "") {
+    sectionsList.push("License");
+  }
+  if (data.contribution !== "") {
+    sectionsList.push("Contribution Guidelines");
+  }
+  sectionsList.push("Tests");
+  sectionsList.push("Questions");
+
+  // Make Contribution Guidlines section if user input exists
   let contributionSection = "";
   if (data.contribution !== "") {
     contributionSection = `## Contribution Guidelines\n\n ${data.contribution}`;
   }
+
+  // Return markdown for the README
   return (
     `# ${data.title}\n\n` +
     renderLicenseBadge(data.license) +
@@ -64,20 +92,16 @@ function generateMarkdown(data) {
 
   ## Description\n\n${data.description}
 
-  ## Table of Contents
-
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Credits](#credits)
-  - [License](#license)
-  
-  ## Installation\n\n${data.installation}
-  
-  ## Usage\n\n${data.usage}\n\n` +
+  ## Table of Contents\n\n` +
+    renderTableOfContents(sectionsList) +
+    `\n\n## Installation\n\n${data.installation}\n\n` +
+    `## Usage\n\n${data.usage}\n\n` +
     renderLicenseSection(data.license) +
     "\n\n---\n\n" +
     contributionSection +
-    `\n\n## Tests\n\n${data.tests}`
+    `\n\n## Tests\n\n${data.tests}` +
+    "\n\n## Questions\n\n" +
+    `You can find my GitHub profile here: https://github.com/${data.username}.\n\nIf you have any further questions you can email me at ${data.email}.`
   );
 }
 
